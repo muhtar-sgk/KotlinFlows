ad8ee080bc93f0a4cc05ac83a47bb8ec243a949e
muhtar-sgk <muhtarrn@gmail.com>
2023-07-13 19:41:53 +0700

    add countdown flow

--- app/src/main/java/com/muhtar/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/muhtar/kotlinflows/MainViewModel.kt
@@ -1,6 +1,35 @@
 package com.muhtar.kotlinflows
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
+    val countDownValue = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            delay(1000L)
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+        viewModelScope.launch {
+            countDownValue.collect{ time ->
+                delay(1500L)
+                println("The current time is $time")
+            }
+        }
+    }
 }
